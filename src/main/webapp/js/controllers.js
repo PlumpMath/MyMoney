04a433d4b991be226fb9471f139162903c6bd3d1
fasfsfgs <fasfsfgs@gmail.com>
2014-09-21 23:10:31 -0300

    Restful service to get tipos;

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -5,37 +5,11 @@
 
 	var myMoneyApp = angular.module('myMoneyApp', []);
 
-	myMoneyApp
-			.controller(
-					'TipoGastoListCtrl',
-					function($scope) {
-						$scope.tipos = [
-								{
-									'codigo' : '1',
-									'descricao' : 'Alimentação',
-									'detalhe' : 'Gastos diretos com alimentação. Por exemplo, restaurantes para almoçar e jantar diariamente. Restaurantes para diversão e ingredientes comprados no supermercado não entram aqui.'
-								},
-								{
-									'codigo' : '2',
-									'descricao' : 'Casa',
-									'detalhe' : 'Gastos para habitar a moradia. Por exemplo, aluguel, produtos de limpeza e produtos de higiene pessoal. Supermercado e drogaria em casa entram aqui.'
-								},
-								{
-									'codigo' : '3',
-									'descricao' : 'Carro',
-									'detalhe' : 'Gastos para usar e manter carro. Por exemplo, lavar e abastecer. Revisões e impostos entram aqui.'
-								},
-								{
-									'codigo' : '4',
-									'descricao' : 'Misc.',
-									'detalhe' : 'Gastos que podem ser cortados. Por exemplo, manter a prática de esportes, restaurantes para diversão e tv a cabo. Isso não significa que esses gastos não são importantes.'
-								},
-								{
-									'codigo' : '5',
-									'descricao' : 'Ensino',
-									'detalhe' : 'Gastos relacionados ao aprimoramento de conhecimentos teóricos e profissionais. Cursos e materiais para o estudo de concursos entram aqui. Livros para diversão não entram aqui.'
-								} ];
+	myMoneyApp.controller('TipoGastoListCtrl', [ '$scope', '$http', function($scope, $http) {
+		$http.get('rest/tipos').success(function(data) {
+			$scope.tipos = data;
+		});
 
-						$scope.orderProp = 'descricao';
-					});
+		$scope.orderProp = 'descricao';
+	} ]);
 })();
--- src/test/resources/webapp/unit/controllersSpec.js
+++ src/test/resources/webapp/unit/controllersSpec.js
@@ -5,19 +5,35 @@
 
 	describe('MyMoney controllers', function() {
 		describe('TipoGastoListCtrl', function() {
-			var scope, ctrl;
+			var scope;
+			var ctrl;
+			var $httpBackend;
 
 			beforeEach(module('myMoneyApp'));
 
-			beforeEach(inject(function($controller) {
-				scope = {};
+			beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+				scope = $rootScope.$new();
+
+				$httpBackend = _$httpBackend_;
+				$httpBackend.expectGET('rest/tipos').respond([ {
+					descricao : 'Oi'
+				}, {
+					descricao : 'Tchau'
+				} ]);
+
 				ctrl = $controller('TipoGastoListCtrl', {
 					$scope : scope
 				});
 			}));
 
-			it('should create "tipos" de gasto with 5 tipos', function() {
-				expect(scope.tipos.length).toBe(5);
+			it('should create "tipos" de gasto with 2 tipos', function() {
+				expect(scope.tipos).toBeUndefined();
+				$httpBackend.flush();
+				expect(scope.tipos).toEqual([ {
+					descricao : 'Oi'
+				}, {
+					descricao : 'Tchau'
+				} ]);
 			});
 
 			it('should set the default value of orderProp model', function() {
